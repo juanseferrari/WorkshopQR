8b1313b89f7c042ec9ef30e7fbf850d8bf487e21
Juan Segundo Ferrari <segundo.ferrari@mercadolibre.com>
2020-09-02 17:51:55 -0300

    arreglos workshop

---
+++ api/index.js
@@ -0,0 +1,122 @@
+const router = require('express').Router();
+const request = require('request');
+const uuidv1 = require('uuid/v1');
+const util = require('util');
+const propertiesReader = require('properties-reader');
+const properties = propertiesReader('./config/properties.conf');
+
+/**
+ * In-memory "database" for storing each order status by his external reference id.
+ * (Real database implementation required on production)
+ */
+var db = [];
+
+const ACCESS_TOKEN = properties.get('access_token'),
+    USER_ID = properties.get('user_id'),
+    POS_ID = properties.get('pos_id'),
+    /**Agregar el resto de la API de publicacion de ordenes en los espacios de XXX con variables declaradas previamente */
+    MP_ORDER_URL = properties.get('mp_order_basepath') + XXX + '/' + XXX + '?access_token=' + XXX,
+    MP_MERCHANT_URL = properties.get('mp_merchant_basepath') + '%d?access_token=' + ACCESS_TOKEN,
+    CURRENCY_ID = properties.get('currency_id');
+
+/**
+ * This resource creates an instore order for item payment
+ */
+router.post('/order', (req, res) => {
+
+    /** Usar las siguientes constantes para el JSON de la orden */
+    const externalReference = POS_ID + '-' + uuidv1();
+    const basePath = req.protocol + '://' + req.get('host');
+    const title = req.body.title
+    const currency_id = CURRENCY_ID
+    const unit_price = req.body.unit_price
+    const quantity = req.body.quantity
+    const picture_url = basePath + req.body.picture_url
+    const notification_url = basePath + '/api/notification'
+
+    let options = {
+        uri: MP_ORDER_URL,
+        method: "POST",
+        json: true,
+        body: {
+            /**Ingresar aqui el JSON para publicar una orden con las constantes mencionadas mas arriba */
+        }
+    }
+
+    request(options, function(err, response, body) {
+
+        if (err || response.statusCode !== 200) {
+            console.log(err);
+            console.log(response.body);
+            return res.sendStatus(500);
+
+        } else {
+            db[externalReference] = 'unknown';
+
+            return res.status(201).json({
+                "order": response.body
+            });
+        }
+    });
+});
+
+/**
+ * This resource cancel an instore order
+ */
+router.delete('/order', (req, res) => {
+    /**Ingresar en el XXX la variable(url) para eliminar una orden (pista: ya la declaramos previamente) */
+    request.delete(XXX, function(err, response, body) {
+
+        if (err || (response.statusCode !== 204 && response.statusCode !== 200)) {
+            console.log(err);
+            console.log(response.body);
+            return res.sendStatus(500);
+
+        } else {
+            return res.sendStatus(204);
+        }
+    });
+});
+
+/**
+ * This resource receives MP notifications for each order update, then retrieve and store its current status. 
+ */
+router.post('/notification', (req, res) => {
+    if (req.query.topic === 'merchant_order') {
+        const id = req.query.id;
+
+        let options = {
+            /** Ingresar en XXX la variable(url) para buscar una merchant_order (pista: ya la declaramos antes) */
+            uri: util.format(XXX, id),
+            /** Ingresar en XXX el metodo para buscar una merchant_order */
+            method: "XXX"
+        }
+
+        request(options, function(err, response, body) {
+
+            if (err || response.statusCode !== 200) {
+                console.log(err);
+                console.log(response.body);
+
+            } else {
+                const order = JSON.parse(response.body);
+                db[order.external_reference] = order.status;
+            }
+        });
+    }
+    /** Ingresar en XXX la respuesta que debe darse al recibir una notificacion */
+    return res.sendStatus(XXX);
+});
+
+/**
+ * This resource returns an order last known status
+ */
+router.get('/status', (req, res) => {
+    const externalReference = req.query.external_reference;
+
+    return res.status(200).json({
+        "status": externalReference in db ? db[externalReference] : 'unknown'
+    })
+});
+
+module.exports = router;
